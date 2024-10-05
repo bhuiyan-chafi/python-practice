c380ae456bf26adeba814e3535b801d447e13765
bhuiyan-chafi <a.bhuiyan@studenti.unipi.it>
2024-10-06 01:01:24 +0200

    06-10-2024/1

--- Basics/Dictionary.py
+++ Basics/Dictionary.py
@@ -16,3 +16,18 @@ print("Printing the whole Dictionary: \n", carModels)
 print("Getting the company only: \n", carModels.get("company"))
 # getting another data from the nested Dictionary
 print("Getting the customer name only: \n", carModels.get("customer", {}).get("name"))
+# removing an item from the list
+carModels.pop("release")
+print("After removing the release attribute: \n", carModels)
+# to remove the entire dictionary we can use del keyword, but we can also delete a specific item from the dictionary using del
+# del carModel
+# print all the key names of the dict
+print("Printing all the keys of the dict:")
+for item in carModels:
+    print(item)
+print("Printing all the values of the dict:")
+for item in carModels.values():
+    print(item)
+print("Printing key and value together: ")
+for item,value in carModels.items():
+    print(item,value)
